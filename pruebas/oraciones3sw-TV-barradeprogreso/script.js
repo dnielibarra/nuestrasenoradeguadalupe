365959a0a3338badc3474f857c8e0ae50488eaae
dnielibarra <65689515+dnielibarra@users.noreply.github.com>
2025-08-07 13:33:16 -0600

    Update script.js

--- pruebas/oraciones3sw-TV-barradeprogreso/script.js
+++ pruebas/oraciones3sw-TV-barradeprogreso/script.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
         "libro1": { icono: '🌱', tema: 'tema-verde', oraciones: ['senalDeLaCruz', 'padreNuestro', 'angelDeLaGuarda'] },
         "libro2": { icono: '🐑', tema: 'tema-amarillo', oraciones: ['senalDeLaCruz', 'padreNuestro', 'angelDeLaGuarda', 'aveMaria'] },
         "libro3":  { icono: '🙏', tema: 'tema-rojo', oraciones: ['senalDeLaCruz', 'padreNuestro', 'angelDeLaGuarda', 'aveMaria', 'gloriaAlPadre'] },
-        "libro4":  { icono: '✨', tema: 'tema-naranja', oraciones: ['senalDeLaCruz', 'padreNuestro', 'angelDeLaGuarda', 'aveMaria', 'gloriaAlPadre'] },
+        "libro4":  { icono: '✨', tema: 'tema-naranja', oraciones: ['senalDeLaCruz', 'padreNuestro', 'angelDeLaGuarda', 'aveMaria', 'gloriaAlPadre' 'credoNiC'] },
         "libro5":  { icono: '📖', tema: 'tema-morado', oraciones: ['senalDeLaCruz', 'padreNuestro', 'angelDeLaGuarda', 'aveMaria', 'gloriaAlPadre', 'credoNiC'] },
         "libro6":  { icono: '🌾', tema: 'tema-granate', oraciones: [] },
         "libro8":  { icono: '⚓', tema: 'tema-indigo', oraciones: [] },
@@ -140,6 +140,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
 
 
 

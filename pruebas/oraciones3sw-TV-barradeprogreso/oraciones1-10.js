eeebc933ba273a84cbc617cc0bf0ba77f24be561
dnielibarra <65689515+dnielibarra@users.noreply.github.com>
2025-08-07 13:20:34 -0600

    Update oraciones1-10.js

--- pruebas/oraciones3sw-TV-barradeprogreso/oraciones1-10.js
+++ pruebas/oraciones3sw-TV-barradeprogreso/oraciones1-10.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
         "libro1": { icono: '🌱', tema: 'tema-verde', oraciones: ['senalDeLaCruz', 'padreNuestro', 'angelDeLaGuarda'] },
         "libro2": { icono: '🐑', tema: 'tema-amarillo', oraciones: ['senalDeLaCruz', 'padreNuestro', 'angelDeLaGuarda', 'aveMaria'] },
         "libro3":  { icono: '🙏', tema: 'tema-rojo', oraciones: ['senalDeLaCruz', 'padreNuestro', 'angelDeLaGuarda', 'aveMaria', 'gloriaAlPadre'] },
-        "libro4":  { icono: '✨', tema: 'tema-naranja', oraciones: [] },
+        "libro4":  { icono: '✨', tema: 'tema-naranja', oraciones: ['senalDeLaCruz', 'padreNuestro', 'angelDeLaGuarda', 'aveMaria', 'gloriaAlPadre'] },
         "libro5":  { icono: '📖', tema: 'tema-morado', oraciones: [] },
         "libro6":  { icono: '🌾', tema: 'tema-granate', oraciones: [] },
         "libro8":  { icono: '⚓', tema: 'tema-indigo', oraciones: [] },
